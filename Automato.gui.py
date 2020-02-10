38fba955cab17e6357322e19e49fe42ec18c0d6d
Izabela Zmk <60762927+IzabelaZmk@users.noreply.github.com>
2020-02-10 03:59:11 +0200

    Add files via upload

---
+++ Automato.gui.py
@@ -0,0 +1,119 @@
+# 09/02/2020
+# Finite Automata Gui Check
+# NEED tkinter Installed
+
+from tkinter import *
+from tkinter import messagebox
+import re
+import sys
+InitialState = []
+TerminalStates = []
+currentState = 0
+m = 0
+patharray = []
+alphabets = []
+states = []
+edges = []
+Input = open("dfa.txt")
+lines = Input.readlines()
+Input.close()
+
+# deleting line breaks and comments..
+i = 0
+while i < len(lines):
+    txt = lines[i]
+    txt = re.sub("\n", "", txt)
+    txt = re.sub("( )*//.*", "", txt)
+    lines[i] = txt
+    i = i+1
+#------------------------------------
+# create arrays with dictionary data
+
+NumofStates = lines[0]
+alphabets = lines[1].split(" ")
+InitialState = lines[2].split(" ")
+TerminalStates = lines[3].split(" ")
+
+i = 4
+j = 0
+while i < len(lines):
+    edge = lines[i].split(" ")
+    edges.append(edge)
+    i = i + 1
+    j = j + 1
+    
+    
+i = 0
+while i < len(edges):
+    if not(edges[i][0] in states):
+        states.append(edges[i][0])
+    if not(edges[i][2] in states):
+        states.append(edges[i][2])
+    
+    i = i + 1
+    
+i = 0
+#------------------------------------
+# scans user's input and check if it is accepted by automata
+
+def run():
+	global m
+	f = 0
+	s = s_storage.get()
+	m = m + 1
+	while f < len(s):
+	    if (s[f] in alphabets):
+	        f = f + 1
+	    else:
+	        messagebox.showerror("Error", "Not Acceptable String You Idiot!")
+	        sys.exit()
+	currentState = InitialState[0]
+	del patharray[:]
+	patharray.append(currentState)
+	for i in range(0, len(s)):
+	    k = 0
+	    while k < j:
+	        if(currentState == edges[k][0] and s[i] == edges[k][1]):
+	            currentState = edges[k][2]
+	            patharray.append(currentState)
+	            k = j
+	        k = k + 1
+
+	f = 0
+	d = 0
+
+	for d in range(len(patharray)): 
+		exec('Label%d=Label(Graphics,text="-> Q%s")\nLabel%d.grid(row=1,column=%d)' % (d+5,patharray[d],d+5,d+1))
+
+	if (currentState in TerminalStates):
+	    Label4 = Label(Graphics, text = "Accepted!")
+	else:
+	    Label4 = Label(Graphics, text = "Rejected!")
+	Label4.grid(row=2,column=1,sticky = W, columnspan = 10)
+
+"""	if m > 1:
+		Label4.grid_forget()
+		for d in range(len(patharray)):
+			exec('Label%d.grid_forget()' % (d+5))"""
+
+Graphics = Tk()
+Graphics.title("Deterministic Finite Automaton")
+Graphics.iconbitmap(r'dfa.ico')
+Graphics.geometry("350x100")
+Graphics.resizable(1,0)
+
+Label1 = Label(Graphics, text = "Enter the word\t: ")
+Label2 = Label(Graphics, text = "The Path is\t: ")
+Label3 = Label(Graphics, text = "Result\t\t: ")
+Button1 = Button(Graphics, text = "Answer",command = run)
+
+s_storage = StringVar()
+s = Entry(textvariable = s_storage)
+
+Label1.grid(row=0,column=0,sticky = W)
+s.grid(row=0,column=1,sticky = W, columnspan = 10)
+Label2.grid(row=1,column=0,sticky = W)
+Label3.grid(row=2,column=0,sticky = W)
+Button1.grid(row=3,column=1,sticky = W, columnspan = 10)
+
+Graphics.mainloop()
